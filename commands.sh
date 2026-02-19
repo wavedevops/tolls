135dcec637991079d32d19ff601f1fcd14737070
Chowdary-Hari <chandrahari321@outlook.com>
2026-02-19 07:15:40 +0530

    create commnds file

---
+++ commands.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+NAMESPACE="prasad"
+
+kubectl create namespace "${NAMESPACE}" --dry-run=client -o yaml | kubectl apply -f -
+kubectl config set-context --current --namespace="${NAMESPACE}"
+
