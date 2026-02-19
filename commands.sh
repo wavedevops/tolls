d46a7b5cbea9652a5d77b2c4ff596d9f74b69e4b
Chowdary-Hari <chandrahari321@outlook.com>
2026-02-19 07:37:54 +0530

    update v2 in file

--- commands.sh
+++ commands.sh
@@ -2,6 +2,12 @@
 
 NAMESPACE="prasad"
 
+echo "alias k=kubectl" >> ~/.bashrc
+source ~/.bashrc
+
+
 kubectl create namespace "${NAMESPACE}" --dry-run=client -o yaml | kubectl apply -f -
 kubectl config set-context --current --namespace="${NAMESPACE}"
+
+
 
