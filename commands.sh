588fd34b574fb4f8ff25a62e16cf7637eb686b59
Chowdary-Hari <chandrahari321@outlook.com>
2026-02-19 07:47:17 +0530

    update v2 in file

--- commands.sh
+++ commands.sh
@@ -2,8 +2,15 @@
 
 NAMESPACE="prasad"
 
-echo "alias k=kubectl" >> ~/.bashrc
+sudo apt update
+sudo apt install -y bash-completion
+
+kubectl completion bash | sudo tee /etc/bash_completion.d/kubectl > /dev/null
+
 source ~/.bashrc
+
+# echo "alias k=kubectl" >> ~/.bashrc
+# source ~/.bashrc
 
 
 kubectl create namespace "${NAMESPACE}" --dry-run=client -o yaml | kubectl apply -f -
